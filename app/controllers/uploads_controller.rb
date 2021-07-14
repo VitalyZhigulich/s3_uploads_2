cedcac16a87ad293703ac757f7d0a91ca9bb9d52
Vitaly Zhigulich <vitko2003@gmail.com>
2021-07-15 02:54:13 +0300

    Base uploads logic

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,8 +1,11 @@
 class UploadsController < ApplicationController
   def create
-    binding.pry
-    true
+    s3 = Aws::S3::Resource.new
+    bucket = s3.bucket(ENV['AWS_S3_BUCKET'])
 
-    @files = []
+    file = params[:file]
+    object = bucket.put_object(body: file, key: file.original_filename)
+
+    @upload_url = object.public_url
   end
 end
